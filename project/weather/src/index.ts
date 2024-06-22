cba1e5ca3103616fbbd01f0262eaaec1d369dccc
haecheonlee <dlgocjs1@gmail.com>
2024-06-22 16:53:30 -0400

    Remove state and hook

--- project/weather/src/index.ts
+++ project/weather/src/index.ts
@@ -1,16 +1,10 @@
 import { App } from "./component/App";
 
-function render() {
-    const app = document.getElementById("app");
+const app = document.getElementById("app");
 
-    if (!app) {
-        throw new Error("The document is not implemented.");
-    }
-
-    while (app.hasChildNodes()) app.firstChild?.remove();
-    app.appendChild(App());
+if (!app) {
+    throw new Error("The document is not implemented.");
 }
 
-render();
-
-export { render };
+while (app.hasChildNodes()) app.firstChild?.remove();
+app.appendChild(App());
