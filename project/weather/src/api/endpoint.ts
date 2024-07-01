b7ac553789c7f7fbd0305848d04a3df3290f1cc8
haecheonlee <dlgocjs1@gmail.com>
2024-06-30 21:06:29 -0400

    enh: add forecast endpoint

--- project/weather/src/api/endpoint.ts
+++ project/weather/src/api/endpoint.ts
@@ -1,3 +1,4 @@
 export enum ApiEndpoints {
-    GET_WEATHER = "/weather?q={city}&appid={apiKey}",
+    GET_WEATHER = "/weather?q={city}&appid={apiKey}&units=metric",
+    GET_FORECAST = "/forecast?q={city}&appid={apiKey}&cnt={count}&units=metric",
 }
