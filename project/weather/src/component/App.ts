0207828c511b901c726ce11a8b8b60b90e5bb6ca
haecheonlee <dlgocjs1@gmail.com>
2024-07-09 19:58:25 -0400

    feat: integrate api client to fetch forecast

--- project/weather/src/component/App.ts
+++ project/weather/src/component/App.ts
@@ -1,10 +1,35 @@
 import { createElement } from "@/core/framework";
 import { SearchComponent } from "./SearchComponent";
 import { WeatherComponent } from "./WeatherComponent";
-import { useState } from "@/core/hook";
+import { useEffect, useState } from "@/core/hook";
+import { ApiEndpoints } from "@/api/endpoint";
+import { ApiService } from "@/api/service";
+import { API_KEY } from "@/api/config";
 
 function App() {
     const [city, setCity] = useState("");
+    const [forecast, setForecast] = useState<ForecastResponse | null>(null);
+
+    useEffect(() => {
+        async function fetchForecast() {
+            if (!API_KEY) {
+                throw new Error("The api key is not provided.");
+            }
+
+            const response = await ApiService.get<ForecastResponse>(
+                ApiEndpoints.GET_FORECAST,
+                {
+                    city: "Montreal",
+                    count: "8",
+                    apiKey: API_KEY,
+                }
+            );
+
+            setForecast(response.data);
+        }
+
+        fetchForecast;
+    }, [city]);
 
     return createElement(
         "div",
