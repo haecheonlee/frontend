e223df1db004d4243de0a64267c8b4db8d639075
haecheonlee <dlgocjs1@gmail.com>
2024-09-01 14:23:35 -0400

    enh: use useOptimistic hook

--- react-19/src/app/page.tsx
+++ react-19/src/app/page.tsx
@@ -1,11 +1,18 @@
 import { Selector } from "@/component/Common";
 import { Action18 } from "@/component/React18";
-import { Action19 } from "@/component/React19";
+import { Action19, OptimisticComponent } from "@/component/React19";
 
 export default function Home() {
     return (
         <main>
-            <Selector react18={<Action18 />} react19={<Action19 />} />
+            <div>
+                <p>Action:</p>
+                <Selector react18={<Action18 />} react19={<Action19 />} />
+            </div>
+            <div>
+                <span>useOptimistic</span>
+                <OptimisticComponent />
+            </div>
         </main>
     );
 }
--- react-19/src/component/React19.tsx
+++ react-19/src/component/React19.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { navigate } from "@/actions";
-import { useActionState } from "react";
+import { useActionState, useOptimistic, useState } from "react";
 
-function updateName(_: string) {
+function updateName(name: string) {
     return new Promise<string>((resolve, reject) => {
         try {
             setTimeout(() => {
-                resolve("");
+                resolve(name);
             }, 1000);
         } catch (e) {
             if (e instanceof Error) {
@@ -38,6 +38,37 @@ export function Action19() {
                 Update
             </button>
             {error && <p>{error}</p>}
+        </form>
+    );
+}
+
+export function OptimisticComponent() {
+    const [name, setName] = useState<string>("");
+
+    return <Optimistic currentName={name} onUpdateName={setName} />;
+}
+
+function Optimistic({ currentName, onUpdateName }: any) {
+    const [optimisticName, setOptimisticName] = useOptimistic(currentName);
+
+    const submitAction = async (formDate: any) => {
+        const newName = formDate.get("name");
+        setOptimisticName(newName);
+        const updatedName = await updateName(newName);
+        onUpdateName(updatedName);
+    };
+
+    return (
+        <form action={submitAction}>
+            <p>Your name is: {optimisticName}</p>
+            <p>
+                <label>Change Name: </label>
+                <input
+                    type="text"
+                    name="name"
+                    disabled={currentName !== optimisticName}
+                />
+            </p>
         </form>
     );
 }
