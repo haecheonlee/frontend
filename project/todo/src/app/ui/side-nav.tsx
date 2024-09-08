55b838211031bc094c47b1916493218b1986cce1
haecheonlee <dlgocjs1@gmail.com>
2024-09-08 19:28:55 -0400

    enh: replace nav-links component

--- project/todo/src/app/ui/side-nav.tsx
+++ project/todo/src/app/ui/side-nav.tsx
@@ -1,8 +1,32 @@
-import { TypographyH3, TypographyXSmall } from "@/components/ui/typography";
-import NavLinks from "./nav-links";
+"use client";
+
+import {
+    TypographyH3,
+    TypographySmall,
+    TypographyXSmall,
+} from "@/components/ui/typography";
 import HorizontalDivider from "./horizontal-divider";
+import { usePathname } from "next/navigation";
+import { DoubleArrowRightIcon, ListBulletIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import clsx from "clsx";
+
+const links = Object.freeze([
+    {
+        name: "Upcoming",
+        href: "/",
+        icon: DoubleArrowRightIcon,
+    },
+    {
+        name: "Today",
+        href: "/today",
+        icon: ListBulletIcon,
+    },
+] as const);
 
 export default function SideNav() {
+    const pathname = usePathname();
+
     return (
         <div className="bg-neutral-800 p-4 rounded-md">
             <div className="flex items-stretch mb-5">
@@ -10,7 +34,21 @@ export default function SideNav() {
             </div>
             <div>
                 <TypographyXSmall isUppercase>Tasks</TypographyXSmall>
-                <NavLinks />
+                {links.map((link) => {
+                    const LinkIcon = link.icon;
+                    return (
+                        <Link
+                            key={link.name}
+                            href={link.href}
+                            className={clsx("flex items-center px-1 py-2", {
+                                "font-bold": link.href === pathname,
+                            })}
+                        >
+                            <LinkIcon className="w-4 h-4 mr-2" />
+                            <TypographySmall>{link.name}</TypographySmall>
+                        </Link>
+                    );
+                })}
             </div>
             <HorizontalDivider />
             <div>
@@ -18,7 +56,7 @@ export default function SideNav() {
             </div>
             <HorizontalDivider />
             <div>
-                <TypographyXSmall isUppercase>Tag</TypographyXSmall>
+                <TypographyXSmall isUppercase>Tags</TypographyXSmall>
             </div>
         </div>
     );
