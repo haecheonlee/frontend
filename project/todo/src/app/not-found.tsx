2d794cd5165c40815f00a3914973a307422a8574
haecheonlee <dlgocjs1@gmail.com>
2024-09-09 19:22:21 -0400

    enh: update not found page

--- project/todo/src/app/not-found.tsx
+++ project/todo/src/app/not-found.tsx
@@ -1,14 +1,14 @@
-import { TypographyH3, TypographySmall } from "@/components/ui/typography";
+import { TypographyH1, TypographySmall } from "@/components/ui/typography";
 
 export default function NotFound() {
     return (
-        <div className="w-full h-full flex justify-center items-center">
-            <div className="text-center">
-                <TypographyH3>404 Page Not Found</TypographyH3>
-                <TypographySmall>
-                    Oops! The page you are looking for does not exist.
-                </TypographySmall>
+        <>
+            <div className="mb-5">
+                <TypographyH1>404 Page Not Found</TypographyH1>
             </div>
-        </div>
+            <TypographySmall>
+                Oops! The page you are looking for does not exist.
+            </TypographySmall>
+        </>
     );
 }
