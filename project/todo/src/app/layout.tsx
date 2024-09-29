b0db984324da914bc61713d1de9773e6ae09ca5a
haecheonlee <dlgocjs1@gmail.com>
2024-09-28 20:35:55 -0400

    fix: prevent to zoom-in when input is focused

--- project/todo/src/app/layout.tsx
+++ project/todo/src/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
     title: "Todo Application",
     description:
         "Create, manage, and track your tasks effortlessly with our user-friendly to-do list app. Boost productivity and stay organized every day.",
-    viewport: "width=device-width, initial-scale=1.0",
+    viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0",
 };
 
 export default function RootLayout({
