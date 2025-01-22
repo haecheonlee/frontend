e74f9995d3507851dcee5bb0fdeb97b0f72889fa
haecheonlee <dlgocjs1@gmail.com>
2025-01-22 17:43:41 -0500

    feat: add initial two sections

--- project/stm/src/app/page.tsx
+++ project/stm/src/app/page.tsx
@@ -1,10 +1,11 @@
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                STM Application
+        <div className="flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="w-full flex-3 flex flex-row items-center sm:items-start">
+                <div className="flex-1 bg-blue-200">Left</div>
+                <div className="flex-3 bg-red-200 overflow-hidden">Right</div>
             </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            <footer className="flex flex-1 gap-6 flex-wrap items-center justify-center">
                 <a
                     href="https://github.com/haecheonlee"
                     target="_blank"
--- project/stm/tailwind.config.ts
+++ project/stm/tailwind.config.ts
@@ -56,6 +56,9 @@ export default {
                 md: "calc(var(--radius) - 2px)",
                 sm: "calc(var(--radius) - 4px)",
             },
+            flex: {
+                "3": "3 3 0%",
+            },
         },
     },
     plugins: [tailwindAnimate],
