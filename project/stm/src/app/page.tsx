92588d6075e904ba3eb1127119d6f4a5389a3515
haecheonlee <dlgocjs1@gmail.com>
2025-01-25 02:59:48 -0500

    feat: update layout with stm logo

--- project/stm/src/app/page.tsx
+++ project/stm/src/app/page.tsx
@@ -1,19 +1,33 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
-        <div className="flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="w-full flex-3 flex flex-row items-center sm:items-start">
-                <div className="flex-1 bg-blue-200">Left</div>
-                <div className="flex-3 bg-red-200 overflow-hidden">Right</div>
-            </main>
-            <footer className="flex flex-1 gap-6 flex-wrap items-center justify-center">
-                <a
-                    href="https://github.com/haecheonlee"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    github
-                </a>{" "}
-            </footer>
+        <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+            <div className="flex flex-col w-[1024px]">
+                <header className="flex flex-row items-center justify-between">
+                    <Image
+                        src="/stm-logo.png"
+                        alt="STM logo"
+                        width={64}
+                        height={64}
+                    />
+                </header>
+                <main className="w-full flex-1 flex flex-row items-center">
+                    <div className="flex-1 bg-blue-200">Left</div>
+                    <div className="flex-3 bg-red-200 overflow-hidden">
+                        Right
+                    </div>
+                </main>
+                <footer className="flex flex-wrap items-center justify-center">
+                    <a
+                        href="https://github.com/haecheonlee"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        github
+                    </a>{" "}
+                </footer>
+            </div>
         </div>
     );
 }
