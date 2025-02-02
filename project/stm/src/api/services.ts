9f7409a73e92cd7677b29eabb19fcf5b223a945a
haecheonlee <dlgocjs1@gmail.com>
2025-02-02 14:52:34 -0500

    feat: implement to process proto file after fetching gtfs-realtime data

--- project/stm/src/api/services.ts
+++ project/stm/src/api/services.ts
@@ -1,11 +1,28 @@
 import { ApiResponse } from "@/types/api";
 import { stmClient } from "./client";
 import { STM_ENDPOINTS } from "./endpoints";
+import path from "path";
+import protobuf from "protobufjs";
 
 type StmClientRequestParameters = {
     endpoint: keyof typeof STM_ENDPOINTS;
     options?: RequestInit;
     timeout?: number;
+};
+
+const processProto = async (arrayBuffer: ArrayBuffer, fileName: string) => {
+    const protoPath = path.join(process.cwd(), "src/data", fileName);
+    const root = await protobuf.load(protoPath);
+
+    const protobufType = root.lookupType("transit_realtime.FeedMessage");
+    const feedMessage = protobufType.decode(new Uint8Array(arrayBuffer));
+
+    return protobufType.toObject(feedMessage, {
+        enums: String,
+        longs: String,
+        bytes: String,
+        defaults: true,
+    });
 };
 
 export const fetchStmClientRequest = async <T>({
@@ -24,10 +41,13 @@ export const fetchStmClientRequest = async <T>({
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
-        const data = await response.json();
+        const decodedProtobufObject = await processProto(
+            await response.arrayBuffer(),
+            "gtfs-realtime.proto"
+        );
 
         return {
-            data: data as T,
+            data: decodedProtobufObject as T,
             success: true,
         };
     } catch (error) {
