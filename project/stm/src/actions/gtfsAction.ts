871379dbfdbf9f9d075f2559b7e4d6170a427cb1
haecheonlee <dlgocjs1@gmail.com>
2025-02-17 18:58:43 -0500

    feat: set return type to getStopsByTripId action

--- project/stm/src/actions/gtfsAction.ts
+++ project/stm/src/actions/gtfsAction.ts
@@ -1,8 +1,11 @@
 import { db } from "@/db/drizzle";
 import { stop_times, stops } from "@/db/schema";
+import { Stops } from "@/types/gtfs";
 import { eq, getTableColumns } from "drizzle-orm";
 
-export const getStopsByTripId = async (tripId: string) => {
+export const getStopsByTripId = async (
+    tripId: string
+): Promise<ReadonlyArray<Readonly<Stops>>> => {
     return await db
         .select({ ...getTableColumns(stops) })
         .from(stops)
