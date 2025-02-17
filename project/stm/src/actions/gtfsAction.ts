fddfd226c917750f44b2db404a5dda6620877125
haecheonlee <dlgocjs1@gmail.com>
2025-02-16 22:11:17 -0500

    feat: implement action to fetch data from db and client to handle db connection

---
+++ project/stm/src/actions/gtfsAction.ts
@@ -0,0 +1,11 @@
+import { db } from "@/db/drizzle";
+import { stop_times, stops } from "@/db/schema";
+import { eq, getTableColumns } from "drizzle-orm";
+
+export const getStopsByTripId = async (tripId: string) => {
+    return await db
+        .select({ ...getTableColumns(stops) })
+        .from(stops)
+        .innerJoin(stop_times, eq(stops.stop_id, stop_times.stop_id))
+        .where(eq(stop_times.trip_id, tripId));
+};
