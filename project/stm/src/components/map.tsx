565979a23d4b30113826c6fd21942b575ff884eb
haecheonlee <dlgocjs1@gmail.com>
2025-03-06 20:44:43 -0500

    feat: show visible markers in map

--- project/stm/src/components/map.tsx
+++ project/stm/src/components/map.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
+import { useGtfs } from "@/context/GtfsContext";
+import { Stops } from "@/types/gtfs";
 
 export default function Map() {
     return (
@@ -18,6 +20,48 @@ export default function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <Markers />
         </MapContainer>
+    );
+}
+
+function Markers() {
+    const map = useMap();
+    const { stops } = useGtfs();
+    const [visibleStops, setVisibleStops] = useState<ReadonlyArray<Stops>>([]);
+
+    useEffect(() => {
+        const updateMarkers = () => {
+            const bounds = map.getBounds();
+
+            setVisibleStops(
+                stops.filter((stop) =>
+                    bounds.contains([
+                        Number(stop.stop_lat),
+                        Number(stop.stop_lon),
+                    ])
+                )
+            );
+        };
+
+        map.on("moveend", updateMarkers);
+        updateMarkers();
+
+        return () => {
+            map.off("moveend", updateMarkers);
+        };
+    }, [map, stops]);
+
+    return (
+        <>
+            {visibleStops.map((stop) => (
+                <Marker
+                    key={stop.stop_id}
+                    position={[Number(stop.stop_lat), Number(stop.stop_lon)]}
+                >
+                    <Popup>{`${stop.stop_name} (${stop.stop_code})`}</Popup>
+                </Marker>
+            ))}
+        </>
     );
 }
