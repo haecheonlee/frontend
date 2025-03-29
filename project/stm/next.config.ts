92e941fd4bb9094a73ff8b19f3a5b7c146c2061c
haecheonlee <dlgocjs1@gmail.com>
2025-03-29 13:02:50 -0400

    fix: remove unused environment variables from next.config.ts

--- project/stm/next.config.ts
+++ project/stm/next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     env: {
-        BASE_URL: process.env.BASE_URL,
         STM_API_URL: process.env.STM_API_URL,
         API_KEY: process.env.API_KEY,
-        CLIENT_SECRET: process.env.CLIENT_SECRET,
         DB_FILE_NAME: process.env.DB_FILE_NAME,
     },
 };
