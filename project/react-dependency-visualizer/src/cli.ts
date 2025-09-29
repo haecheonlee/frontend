a196a708e52d9b3d53030db4645165c0dc691b16
haecheonlee <dlgocjs1@gmail.com>
2025-09-28 20:35:38 -0400

    refactor: replace index.ts by parser.ts

--- project/react-dependency-visualizer/src/cli.ts
+++ project/react-dependency-visualizer/src/cli.ts
@@ -1,5 +1,5 @@
 import { Command } from "commander";
-import { visualizeComponents } from "./index";
+import { start } from "./parser";
 
 const program = new Command();
 
@@ -21,7 +21,7 @@ program
     )
     .action(async (path, options: CommandOptions) => {
         try {
-            await visualizeComponents(path, options);
+            await start(path, options);
         } catch (error) {
             console.log(`An error occurred: ${error}`);
             process.exit(1);
--- project/react-dependency-visualizer/src/parser.ts
+++ project/react-dependency-visualizer/src/parser.ts
@@ -1,9 +1,42 @@
+import fs from "fs";
+import path from "path";
+import { glob } from "fast-glob";
+import { generateHtml } from "./html-generator";
 import { parse } from "@babel/parser";
 import traverse from "@babel/traverse";
-import * as fs from "fs";
-import path from "path";
 
-export function parseComponentFile(filePath: string): ComponentNode {
+export async function start(projectPath: string, options: CommandOptions) {
+    const files = await glob(
+        `${projectPath}/**/${options.rootComponent ?? "*"}.{tsx,jsx}`,
+        {
+            ignore: ["**/node_modules/**"],
+        }
+    );
+
+    if (files.length === 0) {
+        console.error("No React files found in the specified path.");
+        return;
+    }
+
+    const componentData = files.map(parseComponentFile);
+
+    if (options.output) {
+        console.log(`Generating HTML file at ${options.output}...`);
+        const htmlContent = generateHtml(componentData);
+        fs.writeFileSync(options.output, htmlContent);
+        console.log(`HTML file is created at ${options.output}`);
+    }
+
+    componentData.forEach((data) => {
+        console.log(`\nFile: ${data.file}`);
+        console.log(`   Imports: ${data.imports.join(", ")}`);
+        console.log(
+            `   Renders: ${data.renders.map((p) => p.file).join(", ")}`
+        );
+    });
+}
+
+function parseComponentFile(filePath: string): ComponentNode {
     const code = fs.readFileSync(filePath, "utf-8");
     const imports: string[] = [];
     const renders: Component[] = [];
